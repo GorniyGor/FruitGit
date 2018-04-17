2d6746ce615d9a99ba12708081011179574ddcad
Egor Mikhalchenko <gorniygor@gmail.com>
2018-04-17 18:20:12 +0300

    Revert

--- app/src/main/java/ru/anvics/baseproject/presentation/views/ui/fragments/BaseFragment.kt
+++ app/src/main/java/ru/anvics/baseproject/presentation/views/ui/fragments/BaseFragment.kt
@@ -1,20 +1,3 @@
 package ru.anvics.baseproject.presentation.views.ui.fragments
 
-import android.support.v4.app.Fragment
-import ru.anvics.baseproject.presentation.views.BaseView
-import ru.anvics.templates.util.ProgressDialog
-
-abstract class BaseFragment: Fragment(), BaseView {
-    private val progress by lazy {
-        if(activity != null)
-            ProgressDialog(activity!!)
-        else null
-    }
-
-    override fun showProgress() {
-        progress?.show()
-    }
-    override fun hideProgress() {
-        progress?.dismiss()
-    }
-}
+abstract class BaseFragment
