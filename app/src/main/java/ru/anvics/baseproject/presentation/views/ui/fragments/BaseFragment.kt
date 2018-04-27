3fdf70b959e38f7e7cd65b6e959717deced14f19
Egor Mikhalchenko <gorniygor@gmail.com>
2018-04-27 20:14:23 +0300

    Change BaseFragment (add implementation of the function "showProgress", "hideProgress").

--- app/src/main/java/ru/anvics/baseproject/presentation/views/ui/fragments/BaseFragment.kt
+++ app/src/main/java/ru/anvics/baseproject/presentation/views/ui/fragments/BaseFragment.kt
@@ -1,3 +1,20 @@
 package ru.anvics.baseproject.presentation.views.ui.fragments
 
-abstract class BaseFragment
+import android.support.v4.app.Fragment
+import ru.anvics.baseproject.presentation.views.iviews.BaseView
+import ru.anvics.templates.util.ProgressDialog
+
+abstract class BaseFragment: Fragment(), BaseView {
+    private val progress by lazy {
+        if(activity != null)
+            ProgressDialog(activity!!)
+        else null
+    }
+
+    override fun showProgress() {
+        progress?.show()
+    }
+    override fun hideProgress() {
+        progress?.dismiss()
+    }
+}
